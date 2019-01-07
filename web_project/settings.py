65b92791b5727583435cbf4e0c8c874bb3fea3ee
Andrew Novikov <andnovikov@outlook.com>
2019-01-07 14:38:33 +0300

    Fix start error with 'api_card.apps.ApiCardConfig'

--- web_project/settings.py
+++ web_project/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
     'market.apps.MarketConfig',
-    'api_card.apps.ApiCardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
